6d58b2d4d754bd3bf64826f94e1ce642d9821566
Script Raccoon <scriptraccoon@gmail.com>
2025-10-01 03:13:59 +0200

    improve instructions

--- src/lib/Instructions.svelte
+++ src/lib/Instructions.svelte
@@ -1,13 +1,35 @@
 <div>
-	<strong>Instructions.</strong>
-	1. Click between two pieces to bandage them. You can bandage as many pieces as you like.
-	When one piece moves, all pieces connected to it also move. 2. Click on the middle of a
-	piece to make it fixed: it won't be able to move anymore.
+	<h2>Editing Instructions</h2>
+	<ul>
+		<li>
+			Click between two pieces to <strong>bandage</strong> them. You can bandage any
+			number of pieces. When one piece moves, all connected pieces move together.
+		</li>
+		<li>
+			Click the center of a piece to make it <strong>fixed</strong>. Fixed pieces
+			can no longer move.
+		</li>
+		<li>
+			When finished, click the "Share" button to generate a link to your current
+			configuration.
+		</li>
+	</ul>
 </div>
 
 <style>
 	div {
 		color: var(--secondary-font-color);
+	}
+	h2 {
+		font-size: 1rem;
+		font-weight: bold;
+		margin-bottom: 0.75rem;
+	}
+	ul {
+		padding-left: 1.5rem;
+	}
+	li {
+		margin-bottom: 0.5rem;
 		line-height: 1.4;
 	}
 </style>
